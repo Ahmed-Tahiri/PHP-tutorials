3c70944541b1db9da5ddcca0cfaf385ff298a0fe
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-09-12 14:20:29 +0500

    Datatypes in PHP

---
+++ datatypes_php/index.php
@@ -0,0 +1,19 @@
+<?php
+$num_type = 12;
+$string_type = "Ahmed";
+$boolean_type = true;
+$float_type = 10.231;
+$array_type = ["Ahmed", 12, true, 20.21];
+$null_type = null;
+// $resource_type = ftp_connect("127.0.0.1") or die(" Local host not found");//resource is datatype of php in which we can store connections.
+
+
+echo var_dump($num_type);
+echo var_dump($string_type);
+echo var_dump($boolean_type);
+echo var_dump($float_type);
+echo var_dump($array_type);
+echo var_dump($null_type);
+
+
+// another data type object which is used with class, is concept of OOP and will be covered in upcoming lectures
