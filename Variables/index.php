1b45bde7a5d6616ef9e02f349dd031488e9242b6
Ahmed Tahiri <ahmedtahiri.webdev@gmail.com>
2024-09-12 11:40:01 +0500

    first_push

---
+++ Variables/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$name = "Ahmed";
+$age = "21";
+$qualification = "Bachelors in Information Technology";
+
+
+echo "My name is ";
+echo $name;
+echo " My Age is ";
+echo $age;
+echo " My Qualification is ";
+echo $qualification;
